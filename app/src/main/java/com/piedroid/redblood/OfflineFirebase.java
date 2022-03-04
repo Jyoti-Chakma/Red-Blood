4c604637c18914e225145768f4326cc4b41fbc5c
Jyoti Chakma <jyotichakma.official@outlook.com>
2022-03-04 11:02:07 +0600

    Blloda

---
+++ app/src/main/java/com/piedroid/redblood/OfflineFirebase.java
@@ -0,0 +1,14 @@
+package com.piedroid.redblood;
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class OfflineFirebase extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Extra COde
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
